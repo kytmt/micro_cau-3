cf597def11243823265625b03cd00e14a6a0da5e
qotjrdud <robotdr22@cau.ac.kr>
2020-05-31 18:44:08 +0900

    LED

---
+++ led.c
@@ -0,0 +1,27 @@
+#include "led.h"
+
+static short * led;
+
+void init_led(short * address) {
+	led = address;
+}
+
+void led_clear() {
+	*led = (short)~0;
+}
+
+void led_all() {
+	*led = (short)~0xFF;
+}
+
+void led_stage() {
+	if (stage==1){
+    *led = (short)~0x03;
+	}
+	else if (stage==2){
+	*led = (short)~0x1B;
+	}
+	else if (stage==3){
+	*led = (short)~0xDB;
+	}
+}
