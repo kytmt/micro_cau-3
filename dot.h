5cda4149bba69f2e8af0c3d2f71237b7741e01a6
sysysy <cap1053@cau.ac.kr>
2020-05-31 11:18:40 +0900

    dot.h

---
+++ dot.h
@@ -0,0 +1,16 @@
+#ifndef DOT_H
+#define DOT_H
+
+
+#include <unistd.h> // usleep()
+
+
+#define MAX_DOT 5
+
+
+void init_dot(short * address[]);
+void dot_clear();
+void dot_write(int number) ;
+
+
+#endif
