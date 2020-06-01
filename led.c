4bab7cf80d240b5f30033b05d474620a16729de2
qotjrdud <robotdr22@cau.ac.kr>
2020-06-01 18:08:27 +0900

    LED, main파일 수정

--- led.c
+++ led.c
@@ -1,27 +1,53 @@
 #include "led.h"
 
+
+
 static short * led;
 
+
+
 void init_led(short * address) {
+
 	led = address;
+
 }
+
+
 
 void led_clear() {
+
 	*led = (short)~0;
+
 }
+
+
 
 void led_all() {
+
 	*led = (short)~0xFF;
+
 }
 
-void led_stage() {
-	if (sel.temperature==1){
-    *led = (short)~0x03;
+
+
+void led_stage(int stage) {
+
+	if (stage==1){
+
+        *led = (short)~0x03;
+
 	}
-	else if (sel.sidemenu==2){
+
+	else if (stage==2){
+
 	*led = (short)~0x1B;
+
 	}
-	else if (sel.payment==3){
+
+	else if (stage==3){
+
 	*led = (short)~0xDB;
+
 	}
+
 }
